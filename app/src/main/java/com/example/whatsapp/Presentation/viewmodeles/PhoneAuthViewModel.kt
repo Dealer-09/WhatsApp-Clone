431445c11473ee895f7dfadd1cf70f3914191f72
Aditya Kumar Mishra <vadityamishra777@gmail.com>
2025-05-25 15:25:12 +0530

    push

--- app/src/main/java/com/example/whatsapp/Presentation/viewmodeles/PhoneAuthViewModel.kt
+++ app/src/main/java/com/example/whatsapp/Presentation/viewmodeles/PhoneAuthViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.whatsapp.Presentation.viewmodeles
 
+import android.app.Activity
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.whatsapp.Model.PhoneAuthUser
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.FirebaseDatabase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,12 +16,45 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class PhoneAuthViewModel @Inject constructor(): ViewModel() {
+class PhoneAuthViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val database: FirebaseDatabase
+    )
+    : ViewModel() {
 
-    private val _authState = MutableStateFlow <AuthState>(AuthState.Ideal)
-  val authState = _authState.asStateFlow()
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Ideal)
+    val authState = _authState.asStateFlow()
+
+    private val userRef = database.reference.child("users")
+
+
+    fun sendVerificationCode(phoneNumber: String, activity: Activity) {
+
+        _authState.value = AuthState.Loading
+
+        val option = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
+                super.onCodeSent(id, token)
+
+                Log.d("PhoneAuth","onCodeSent triggered.verification ID: $id")
+                _authState.value = AuthState.CodeSent(verificationId= id)
+            }
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onVerificationFailed(exception: FirebaseException) {
+               Log.e("PhoneAuthUser","Verification failed :${exception.message}")
+                _authState.value = AuthState.Error(exception.message ?: "Verification failed")
+
+            }
+
+
+        }
     }
-
+}
 
 sealed class AuthState{
     object Ideal:AuthState()
